8eff01cd2986c7ac8061e00ab052e25c951ec5d1
Stéphane <steph13.desire@gmail.com>
2022-10-19 17:34:28 +0200

    Creation de la classe Parking qui comprend les différentes informations utiles pour l'utilisateur

---
+++ parkapp/src/main/java/com/parkapp/parkapp/model/Parking.java
@@ -0,0 +1,79 @@
+package com.parkapp.parkapp.model;
+
+public class Parking {
+
+    private String name;
+    private double longitude;
+    private double latitude;
+    private int nbPlaceInitial;
+    private int nbPlaceRestante;
+    private double distanceUtilisateur;
+
+    public Parking(String name, double longitude, double latitude, int nbPlaceInitial) {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.nbPlaceInitial = nbPlaceInitial;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getNbPlaceInitial() {
+        return nbPlaceInitial;
+    }
+
+    public void setNbPlaceInitial(int nbPlaceInitial) {
+        this.nbPlaceInitial = nbPlaceInitial;
+    }
+
+    public int getNbPlaceRestante() {
+        return nbPlaceRestante;
+    }
+
+    public void setNbPlaceRestante(int nbPlaceRestante) {
+        this.nbPlaceRestante = nbPlaceRestante;
+    }
+
+    public double getDistanceUtilisateur() {
+        return distanceUtilisateur;
+    }
+
+    public void setDistanceUtilisateur(double distanceUtilisateur) {
+        this.distanceUtilisateur = distanceUtilisateur;
+    }
+
+    @Override
+    public String toString() {
+        return "Parking{" +
+                "name='" + name + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", nbPlaceInitial=" + nbPlaceInitial +
+                ", nbPlaceRestante=" + nbPlaceRestante +
+                ", distanceUtilisateur=" + distanceUtilisateur +
+                '}';
+    }
+}
+
