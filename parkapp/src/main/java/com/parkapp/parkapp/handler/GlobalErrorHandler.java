a4b36205a536511dce7883fb5975bf607cc6a877
Stéphane <steph13.desire@gmail.com>
2022-10-21 10:19:04 +0200

    Update du ControllerAdvice

--- parkapp/src/main/java/com/parkapp/parkapp/handler/GlobalErrorHandler.java
+++ parkapp/src/main/java/com/parkapp/parkapp/handler/GlobalErrorHandler.java
@@ -1,6 +1,9 @@
 package com.parkapp.parkapp.handler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.parkapp.parkapp.controller.ParkingController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalErrorHandler {
 
-    @ExceptionHandler({JsonProcessingException.class})
-    public ResponseEntity<String> handleJsonProcessingException(JsonProcessingException e){
-        return  new ResponseEntity<>("Erreur serveur", HttpStatus.INTERNAL_SERVER_ERROR);
+    private static final Logger LOG = LoggerFactory.getLogger(ParkingController.class);
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleException(Exception e){
+        LOG.error(e.getMessage());
+        return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
